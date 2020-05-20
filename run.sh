766e6b32f23a9447ff82c29fbc019c5411a71cb9
sandeep <sandeep06011991@gmail.com>
2020-05-19 22:26:53 -0400

    data to edgelists

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
-export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64
-rm experiments.txt
-python experiment/epoch_run_time.py ./example_data/toy-ppi
-python experiment/epoch_run_time.py ./example_data/reddit/reddit
-python experiment/epoch_run_time.py ./example_data/ppi/ppi
+#export LD_LIBRARY_PATH=/usr/local/cuda-9.0/lib64
+#rm experiments.txt
+#python experiment/epoch_run_time.py ./example_data/toy-ppi
+#python experiment/epoch_run_time.py ./example_data/reddit/reddit
+#python experiment/epoch_run_time.py ./example_data/ppi/ppi
+python experiment/data_post_process.py ./example_data/toy-ppi
 #python -m graphsage.supervised_train --train_prefix ./example_data/reddit/reddit --model graphsage_maxpool --sigmoid
 #python -m graphsage.supervised_train --train_prefix ./example_data/ppi/ppi --model graphsage_maxpool --sigmoid
 #python -m graphsage.unsupervised_train --train_prefix ./example_data/reddit/reddit --model gcn --max_total_steps 1000 --validate_iter 10
