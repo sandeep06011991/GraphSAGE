6be7fe839fbb338b7556c28601da84f0b05d9685
sandeep <sandeep06011991@gmail.com>
2020-05-26 22:29:55 -0400

    added sampling code

--- experiment/tensorflow_sampling.py
+++ experiment/tensorflow_sampling.py
@@ -3,8 +3,86 @@ import numpy as np
 import sys,os
 from os import path
 sys.path.insert(0, os.getcwd())
+
+def use_constant_adj_matrix(dataset,adj_matrix,train_nodes,gpu_avail):
+    from graphsage.neigh_samplers import UniformNeighborSampler
+    import tensorflow as tf
+    a,b = (adj_matrix.shape)
+    adj_matrix_constant = tf.constant(adj_matrix)
+    adj_matrix_constant = tf.cast(adj_matrix_constant,tf.int32)
+    sampler = UniformNeighborSampler(adj_matrix)
+    batch =  tf.placeholder(tf.int32, shape=(None,), name='batch')
+    sample1 = sampler((batch, 25))
+    sample1 = tf.cast(sample1, dtype=tf.int64)
+    s_tv = tf.reshape(sample1, [tf.shape(sample1)[0] * 25, ])
+    sample2 = sampler((s_tv, 10))
+    sess = tf.Session()
+    train_nodes =  np.random.permutation(train_nodes)
+    import time
+    start_time = time.time()
+    i = 0
+    batchsize = 1
+    interrupted = False
+    while  i < len(train_nodes):
+        offset = min(len(train_nodes),i+batchsize)
+        feed_dict= {batch:train_nodes[i:offset]}
+        sess.run(sample2, feed_dict)
+        i = i+offset
+        if time.time()-start_time > 120:
+            interrupted = True
+            break
+    end_time = time.time()
+    sess.close()
+    with open("tf_measurements.txt","a") as fp:
+        print("{} | {} | {} | {} | {} \n".format(dataset,gpu_avail,"CONSTANT",interrupted,end_time-start_time))
+        if not interrupted:
+            fp.write("{} | {} | {} | {} | {} \n".format(dataset,gpu_avail,"CONSTANT",interrupted,end_time-start_time))
+        else:
+            fp.write("{} | {} | {} | {} | {} \n".format(dataset,gpu_avail,"CONSTANT",interrupted,(end_time-start_time)*(len(train_nodes)/offset)))
+
+
+def use_feed_dict_matrix(dataset,adj_matrix,train_nodes,gpu_avail):
+    from graphsage.neigh_samplers import UniformNeighborSampler
+    import tensorflow as tf
+    adj_matrixph = tf.placeholder(tf.int32,shape=adj_matrix.shape,name='adj_matrix')
+    sampler = UniformNeighborSampler(adj_matrixph)
+    batch =  tf.placeholder(tf.int32, shape=(None,), name='batch')
+    sample1 = sampler((batch, 25))
+    sample1 = tf.cast(sample1, dtype=tf.int64)
+    s_tv = tf.reshape(sample1, [tf.shape(sample1)[0] * 25, ])
+    sample2 = sampler((s_tv, 10))
+    sess = tf.Session()
+    train_nodes =  np.random.permutation(train_nodes)
+    import time
+    start_time = time.time()
+    i = 0
+    batchsize = 1
+    interrupted = False
+    while  i < len(train_nodes):
+        offset = min(len(train_nodes),i+batchsize)
+        feed_dict= {batch:train_nodes[i:offset],adj_matrixph:adj_matrix}
+        sess.run(sample2, feed_dict)
+        i = i+offset
+        if time.time()-start_time > 120:
+            interrupted = True
+            break
+    end_time = time.time()
+    sess.close()
+    with open("tf_measurements.txt","a") as fp:
+        print("{} | {} | {} | {} | {} \n".format(dataset,gpu_avail,"FEEDDICT",interrupted,(end_time-start_time)*(len(train_nodes)/offset)))
+        if not interrupted:
+            fp.write("{} | {} | {} | {} | {} \n".format(dataset,gpu_avail,"FEEDDICT",interrupted,end_time-start_time))
+        else:
+            fp.write("{} | {} | {} | {} | {} \n".format(dataset,gpu_avail,"FEEDDICT",interrupted,(end_time-start_time)*(len(train_nodes)/offset)))    
+
 def run():
     dataset = sys.argv[1]
+    gpu = sys.argv[2]
+
+    if gpu == "gpu":
+        os.environ["CUDA_VISIBLE_DEVICES"]="0"
+    else:
+        os.environ["CUDA_VISIBLE_DEVICES"]="-1"
     fp = open(dataset,'r')
     nodes = {}
     maxv = 0
@@ -33,32 +111,16 @@ def run():
         elif len(neighbors) < max_degree:
             neighbors = np.random.choice(neighbors, max_degree, replace=True)
         adj_matrix[n , :] = neighbors
-    from graphsage.neigh_samplers import UniformNeighborSampler
+    a,b = (adj_matrix.shape)
+    gpu_constant_possible = False
+    if a*b < 2 * 1024 * 1024 * 1024 / 4 :
+        gpu_constant_possible = True
     import tensorflow as tf
-    print(adj_matrix.shape)
-    adj_matrixph = tf.placeholder(tf.int32,shape=adj_matrix.shape,name='adj_matrix')
-    sampler = UniformNeighborSampler(adj_matrixph)
-    batch =  tf.placeholder(tf.int32, shape=(None,), name='batch')
-    sample1 = sampler((batch, 25))
-    sample1 = tf.cast(sample1, dtype=tf.int64)
-    s_tv = tf.reshape(sample1, [tf.shape(sample1)[0] * 25, ])
-    sample2 = sampler((s_tv, 10))
-    sess = tf.Session()
-    train_nodes =  np.random.permutation(train_nodes)
-    import time
-    start_time = time.time()
-    i = 0
-    batchsize = 1
-    while  i < len(train_nodes):
-        offset = min(len(train_nodes),i+batchsize)
-        feed_dict= {batch:train_nodes[i:offset],adj_matrixph:adj_matrix}
-        sess.run(sample2, feed_dict)
-        i = i+offset
-    end_time = time.time()
-    sess.close()
-    print("Time measured for {} is {}".format(dataset,end_time-start_time))
-    with open("tf_measurements.txt","w") as fp:
-        fp.write("{} | {}".format(dataset,end_time-start_time))
+    gpu_avail = tf.test.is_gpu_available()
+
+    use_feed_dict_matrix(dataset,adj_matrix,train_nodes,gpu_avail)
+    if gpu_constant_possible:
+        use_constant_adj_matrix(dataset,adj_matrix,train_nodes,gpu_avail)
 
 if __name__ == "__main__":
     run()
